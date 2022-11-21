f7be1ede0245e8e21f2bb4b3ee9f2697f9d8dbd7
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-11-21 09:01:27 +0700

    menambah beberapa routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 const crypto = require('crypto');
 const User = require('../models/userModel');
 
@@ -8,15 +9,16 @@ const getHashedPassword = (password) => {
 };
 
 module.exports = {
-
+  // @desc me redirect ke halaman form untuk  register
   formRegister(req, res) {
     // res.sendFile('form.html', { root: 'views' });
     res.render('form');
   },
+  // @desc menambahkan data user ke database
   async createUser(req, res) {
     try {
       const hashedPassword = getHashedPassword(req.body.password);
-      const student = await User.create({
+      const user = await User.create({
         name: req.body.name,
         password: hashedPassword,
         email: req.body.email,
@@ -26,7 +28,73 @@ module.exports = {
       res.status(200).json({
         succes: true,
         message: 'berhasil menambahkan data mahasiswa',
-        data: student,
+        data: user,
+      });
+    } catch (err) {
+      res.status(400).json({
+        succes: false,
+        message: err.message,
+      });
+    }
+  },
+  // @desc mengambil seluruh data user
+  async getAllUsers(req, res) {
+    try {
+      const user = await User.findAll();
+
+      res.status(200).json({
+        succes: true,
+        message: 'melihat seluruh data mahasiswa',
+        data: user,
+      });
+    } catch {
+      res.status(500).json({
+        succes: false,
+        message: 'something went wrong',
+      });
+    }
+  },
+  // @desc melihat data satu user
+  async getUser(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(404).json({
+          succes: false,
+          message: 'data mahasiswa tidak ditemukan',
+        });
+      }
+
+      res.status(200).json({
+        succes: true,
+        message: `melihat data mahasiswa ke-${req.params.id}`,
+        data: user,
+      });
+    } catch (err) {
+      res.status(400).json({
+        succes: false,
+        message: err.message,
+      });
+    }
+  },
+  // @desc memhapus data user
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(404).json({
+          succes: false,
+          message: 'data user tidak ditemukan',
+        });
+      }
+
+      await user.destroy();
+
+      res.status(200).json({
+        succes: true,
+        message: 'data userberhasil terhapus',
       });
     } catch (err) {
       res.status(400).json({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,20 @@
 const express = require('express');
 
 const router = express.Router();
-const { formRegister, createUser } = require('../controllers/userController');
+const {
+  formRegister,
+  createUser,
+  formLogin,
+  loginUser,
+  getAllUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+} = require('../controllers/userController');
 
 router.route('/').get(formRegister).post(createUser);
+router.route('/login').get(formLogin).post(loginUser);
+router.route('/allusers').get(getAllUsers);
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
