a783b204cb7c55319ff603793854cc65df6d74b4
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-11-29 09:54:34 +0700

    fix bugs host value

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,7 @@ const { DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD } = process.env;
 
 const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, {
   dialect: 'mysql',
-  host: 'localhost',
+  host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
   timezone: '+07:00',
 });
 
